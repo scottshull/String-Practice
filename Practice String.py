eadec7b5b798eaa66443b02ac21394896b9ab0d2
scottshull <sshull@students.mchenry.edu>
2016-09-29 13:48:41 -0500

    Add files via upload

---
+++ Practice String.py
@@ -0,0 +1,28 @@
+school = "Mchenry Country College"
+latter = school[0]
+print letter
+
+school = "Mchenry Country College"
+len(school)
+print len(school)
+
+index = 0 while index < len(school):
+    letter = school[index]
+    print letter
+    index += 1
+
+school = "Mchenry Country College"
+print school[0:7]
+print school[8:14]
+print school[15:22]
+
+word = "Mchenry Country College"
+count = 0
+for letter in word
+    if letter == 'y':
+        count+= 1
+print count
+
+word = "college"
+new_word = word.upper()
+print new_word
